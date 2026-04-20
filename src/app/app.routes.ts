92065984ab57ad2517a2de6f490a878784a3d282
Raul Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2026-04-21 00:36:35 +0200

    feat: update routes and nav for alianzas, personajes, and unified list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./components/prophecies-list/prophecies-list').then(m => m.PropheciesList),
+    loadComponent: () => import('./components/app-list/app-list').then(m => m.AppList),
+    data: { entityType: 'prophecy' },
   },
   {
     path: 'prophecy/:id',
@@ -13,13 +13,33 @@ export const routes: Routes = [
   },
   {
     path: 'references',
-    loadComponent: () =>
-      import('./components/references-list/references-list').then(m => m.ReferencesList),
+    loadComponent: () => import('./components/app-list/app-list').then(m => m.AppList),
+    data: { entityType: 'reference' },
   },
   {
     path: 'reference/:id',
     loadComponent: () =>
       import('./components/reference-detail/reference-detail').then(m => m.ReferenceDetail),
+  },
+  {
+    path: 'covenants',
+    loadComponent: () => import('./components/app-list/app-list').then(m => m.AppList),
+    data: { entityType: 'covenant' },
+  },
+  {
+    path: 'covenant/:id',
+    loadComponent: () =>
+      import('./components/covenant-detail/covenant-detail').then(m => m.CovenantDetail),
+  },
+  {
+    path: 'network',
+    loadComponent: () =>
+      import('./components/character-network/character-network').then(m => m.CharacterNetwork),
+  },
+  {
+    path: 'character/:id',
+    loadComponent: () =>
+      import('./components/character-detail/character-detail').then(m => m.CharacterDetail),
   },
   {
     path: 'charts',
